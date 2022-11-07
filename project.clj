6a63f43c5fd776cf1f2bdc3c67e103e3f929d1b6
Tobias Johansson <tobias.johansson@gmail.com>
2022-11-07 09:13:21 +0100

    added dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject com.infolace/excel-templates "0.3.3"
+(defproject com.infolace/excel-templates "0.3.3-mt2"
   :description "Build Excel files by combining a template with plain old data"
   :url "https://github.com/tomfaulhaber/excel-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.commons/commons-lang3 "3.12.0"]
+                 [commons-io/commons-io "2.11.0"]
+                 [org.apache.poi/poi-ooxml "5.2.3"]
                  [org.apache.poi/poi-ooxml-full "5.2.3"] 
                  [org.clojure/data.zip "1.0.0"]
                  [joda-time "2.12.0"]]
